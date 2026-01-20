710d2f8782c54674da8224f3feff872b59269e8b
Yoder the coder <yonas.ermias@student.northampton.edu>
2026-01-20 19:04:39 +0000

    input and add two integers

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,15 @@
-// YOUR NAME HERE
+// YONAS ERMIAS
 #include <iostream>
 using namespace std;
 
 int main () {
+    int x, y;
+
+    cout << "Enter two integers ";
+    cin >> x >> y;
+
+    cout << "The sum is " << x+y << endl;
+
+return 0;
 
 }
